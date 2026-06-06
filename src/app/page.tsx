cc6d34d13cf39e29d3ecb8d25bd473570289804c
gomugomucode <alinedtwins@gmail.com>
2026-06-06 20:11:49 +0545

    fix: comment out Reviews component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <SatisfactionBanner />
         <AboutSection />
         <Products />
-        <Reviews />
+        {/* <Reviews /> */}
         <Clients />
         <ContactForm />
       </main>
